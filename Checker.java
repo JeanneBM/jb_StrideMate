b076bb17085c4af21df7c9973346460ec65fb582
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2025-03-04 23:56:39 +0100

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -1,4 +1,4 @@
-//przewidywany czas 02.11.; 03.03-17.03
+//przewidywany czas 02.11.; 03.03-17.03; https://www.facebook.com/aquaparkwroclaw/photos/karnet-super-vip-4-miesi%C4%85ce-w-cenie-3-sauny-%EF%B8%8F-baseny-fitness-si%C5%82ownia-%EF%B8%8Fkupuj%C4%85c-k/1076961824470788/?_rdr
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
